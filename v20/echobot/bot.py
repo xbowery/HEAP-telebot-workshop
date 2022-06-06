76a3cd2e68a72e7bcdcb2becba7b126cf0d23023
xbowery <69230356+xbowery@users.noreply.github.com>
2022-06-06 22:57:17 +0800

    Created echobot for v20

---
+++ v20/echobot/bot.py
@@ -0,0 +1,59 @@
+import os
+import logging
+import signal
+import telegram
+from dotenv import load_dotenv
+
+from telegram import Update
+from telegram.ext import (
+    Application,
+    CallbackContext,
+    CommandHandler,
+    MessageHandler,
+    filters,
+)
+
+# Enable logging
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO
+)
+logger = logging.getLogger(__name__)
+
+load_dotenv("../.env")
+TOKEN = os.getenv("token")
+
+async def start(update:Update, context:CallbackContext.DEFAULT_TYPE) -> None:
+    """Send a message when the command /start is issued"""
+
+    msg = (
+        "Hello! Thanks for using my bot! This bot echoes whatever"
+        " messages that you send to it. Hope you have a good day ahead!"
+    )
+    await update.message.reply_text(msg)
+
+async def echo(update: Update, context: CallbackContext) -> None:
+    """Echoes the user message"""
+    message = update.message.text
+    await update.message.reply_text(message)
+
+async def error(update: Update, context: CallbackContext) -> None:
+    """Log errors caused by updates"""
+    logger.warning("Update '%s' caused error '%s'", update, context.error)
+
+def main():
+    """Run the bot."""
+    # Create the Application and pass it your bot's token
+    application = Application.builder().token(TOKEN).build()
+
+    # Add Command handlers
+    application.add_handler(CommandHandler("start", start))
+
+    # Add Message handlers
+    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
+
+    # Run the bot until user presses Ctrl-C
+    application.run_polling()
+
+if __name__ == "__main__":
+    main()
